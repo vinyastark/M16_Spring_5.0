d3e41b4fff7d4d9392f26bf251a76e3b6042808f
user <user@LAPTOP-QUJ0FFGN>
2022-06-24 21:36:52 +0530

    First Commit

---
+++ SpringBootProject/src/main/java/com/example/demo/SpringBootProjectApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.entity.Student;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext c= SpringApplication.run(SpringBootProjectApplication.class, args);
+		Student s=c.getBean(Student.class);
+		s.display();
+	}
+
+}
