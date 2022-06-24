4c42f0a6e63c6ee2696cf9e9e9effce6947fbf51
user <user@LAPTOP-QUJ0FFGN>
2022-06-25 00:09:09 +0530

    First Commit

---
+++ SpringMVC-2/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/admin")
+	public String home(HttpServletRequest req,HttpServletResponse res)
+	{
+		HttpSession session=req.getSession();
+		String name=req.getParameter("name");
+		System.out.println("Welcome To TechnoServe India "+name);
+		session.setAttribute("name", name);
+		return "home.jsp";
+	}
+	
+	
+}
